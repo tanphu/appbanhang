6492056ef746f3df9e70e4fa1cd01b5957a6e382
lysyaoran25 <33797033+lysyaoran25@users.noreply.github.com>
2018-12-18 23:24:54 +0700

    Revert "Revert "Revert "Update Cart.js"""
    This reverts commit 9409f28d1faebc77d7ba4f0ca126162aa0eba7ee.

--- Screen/Main/Cart.js
+++ Screen/Main/Cart.js
@@ -1,25 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, AsyncStorage } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { connect } from 'react-redux'
 class Cart extends React.Component {
   static navigationOptions = {
     title: "Giỏ hàng",
 
-  };
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      userinfo: null
-    };
-  }
-
-  componentDidMount() {
-    AsyncStorage.getItem("User").then(value => {
-      if (value) {
-        this.setState({ userinfo: JSON.parse(value) });
-      }
-    })
   };
 
   renderProducts = (products) => {
@@ -33,18 +18,9 @@ class Cart extends React.Component {
     })
   }
 
-  rendersss = () => {
-    return (
-      <Text>{this.state.userinfo.name}</Text>
-    )
-  }
-
   render() {
-
     return (
-
       <View style={styles.container}>
-        {this.state.userinfo ? (this.rendersss())  : null}
         <Text> Cart screen </Text>
         {this.renderProducts(this.props.cartItems)}
       </View>
