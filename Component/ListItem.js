eac177c553cc0810554e290a2e357b9d204ddb8b
tanphu <31039392+tanphu@users.noreply.github.com>
2018-12-18 23:44:16 +0700

    Share button

--- Component/ListItem.js
+++ Component/ListItem.js
@@ -16,19 +16,30 @@ class ListItems extends React.Component {
     btnFavorite = () => {
         this.setState({ select: !this.state.select })
     }
+
+    OnShare = (item) => {
+        Share.share({
+            title: 'share',
+            url: item.image.i1,
+            message: item.image.i1,
+        }, {
+                dialogTitle: 'Share this awesome content'
+            })
+    }
+
     render() {
         return (
 
-            <TouchableOpacity style={styles.container} onPress={()=>{this.props.navigation.navigate('Detail', {item: this.props.item})}}>
+            <TouchableOpacity style={styles.container} onPress={() => { this.props.navigation.navigate('Detail', { item: this.props.item }) }}>
                 <ImageBackground
-                    style={{ width: ITEM_WIDTH / 2 -10, height: ITEM_HEIGHT /2 - 150 }}
+                    style={{ width: ITEM_WIDTH / 2 - 10, height: ITEM_HEIGHT / 2 - 150 }}
                     source={{ uri: this.props.item.image.i1 }}
                 >
-                    <View style={{ flexDirection: 'row-reverse', justifyContent: 'space-between', height: 35 , paddingTop:5 }}>
+                    <View style={{ flexDirection: 'row-reverse', justifyContent: 'space-between', height: 35, paddingTop: 5 }}>
                         <TouchableOpacity onPress={this.btnFavorite}>
                             <Entypo name={this.state.select ? 'heart' : 'heart-outlined'} size={30} color={this.state.select ? '#B39168' : '#004F92'} />
                         </TouchableOpacity>
-                        <TouchableOpacity>
+                        <TouchableOpacity onPress={() => this.OnShare(this.props.item)}>
                             <Entypo name='share' size={30} color='#004F92' />
                         </TouchableOpacity>
                     </View>
@@ -37,7 +48,7 @@ class ListItems extends React.Component {
                     <Text>{this.props.item.name}</Text>
                     <Text>{this.props.item.trademark}</Text>
                     <Text style={{ fontSize: 15, color: '#ff0000' }}>{this.props.item.price} VND</Text>
-                    <AddCart item={this.props.item}/>
+                    <AddCart item={this.props.item} />
                 </View>
 
             </TouchableOpacity>
