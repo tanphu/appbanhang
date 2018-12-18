d5cbb503d142f60947720f4ba761609469c1a4c0
lysyaoran25 <33797033+lysyaoran25@users.noreply.github.com>
2018-12-18 22:54:31 +0700

    Add item to Cart

--- Screen/Main/Cart.js
+++ Screen/Main/Cart.js
@@ -1,21 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { connect } from 'react-redux'
 class Cart extends React.Component {
   static navigationOptions = {
     title: "Giỏ hàng",
 
   };
+
+  renderProducts = (products) => {
+
+    return products.map((item, index) => {
+      return (
+        <View key={index} style={{ padding: 20 }}>
+          <Button title={item.name + " - "} />
+        </View>
+      )
+    })
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text> Cart screen </Text>
+        {this.renderProducts(this.props.cartItems)}
       </View>
     );
   }
 }
 
-export default Cart;
+
+const mapStateToProps = (state) => {
+  return {
+    cartItems: state
+  }
+}
+
+export default connect(mapStateToProps)(Cart);
 
 const styles = StyleSheet.create({
   container: {
